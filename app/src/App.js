2dba64a6ba893faf9da70eda79ca13ad5f2c0251
an.le <avehouse8@gmail.com>
2023-05-02 15:59:49 +0700

    Wei to Ether Conversion

--- app/src/App.js
+++ app/src/App.js
@@ -29,7 +29,7 @@ function App() {
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    const value = ethers.utils.parseUnits(document.getElementById('eth').value,"ether");
     const escrowContract = await deploy(signer, arbiter, beneficiary, value);
 
 
@@ -37,7 +37,7 @@ function App() {
       address: escrowContract.address,
       arbiter,
       beneficiary,
-      value: value.toString(),
+      value: `${document.getElementById('eth').value} ether`,
       handleApprove: async () => {
         escrowContract.on('Approved', () => {
           document.getElementById(escrowContract.address).className =
@@ -70,8 +70,8 @@ function App() {
         </label>
 
         <label>
-          Deposit Amount (in Wei)
-          <input type="text" id="wei" />
+          Deposit Amount (in Eth)
+          <input type="text" id="eth" />
         </label>
 
         <div
